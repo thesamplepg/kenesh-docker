1c1ab2541a0d33bb919f99f45bab044ab2f339f9
thesamplepg <thesamplepg@gmail.com>
2024-10-24 15:14:24 +0600

    fixed bugs

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -2,10 +2,10 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
+  "strapi::cors",
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
       origin: [
         process.env.CLIENT_ORIGIN || "http://localhost:3000",
         process.env.SERVER_ORIGIN || "http://127.0.0.1:1337",
